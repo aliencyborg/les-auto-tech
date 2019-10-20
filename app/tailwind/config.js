17516c2fa25cecabace277d4b4a375afc5999fbe
Benjamin Rosas <ben@aliencyb.org>
2019-10-20 14:21:08 -0500

    install and setup postcss and tailwind

---
+++ app/tailwind/config.js
@@ -0,0 +1,33 @@
+/* global module */
+
+module.exports = {
+  theme: {
+    extend: {},
+    fontFamily: {
+      // Tailwind defaults:
+      sans: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Helvetica Neue',
+        'Arial',
+        'Noto Sans',
+        'sans-serif',
+        'Apple Color Emoji',
+        'Segoe UI Emoji',
+        'Segoe UI Symbol',
+        'Noto Color Emoji'
+      ],
+      // serif: 'Georgia, Cambria, Times New Roman, Times, serif',
+      mono: 'Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace',
+
+      // My fonts
+      base: 'acumen-pro, sans-serif',
+      logo: 'cooper-black-std, serif',
+      serif: 'kepler-std, Times, serif'
+    }
+  },
+  variants: {},
+  plugins: []
+}
