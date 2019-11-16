4e25e51466fa4007bf6dbf0a9b46cb810a7047d4
Benjamin Rosas <ben@aliencyb.org>
2019-11-16 09:45:21 -0600

    footer

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,10 @@ import Controller from '@ember/controller'
 export default class ApplicationController extends Controller {
   originalYear = 1981
 
+  get currentYear() {
+    return new Date().getFullYear()
+  }
+
   getCurrentYear() {
     return new Date().getFullYear()
   }
