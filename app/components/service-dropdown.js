ad2051daa4675d18b44af77c12b15971176ec470
Benjamin Rosas <ben@aliencyb.org>
2019-11-17 20:42:22 -0600

    updates to services and general layout tweaks

--- app/components/service-dropdown.js
+++ app/components/service-dropdown.js
@@ -3,13 +3,24 @@ import { action } from '@ember/object'
 import { tracked } from '@glimmer/tracking'
 
 export default class ServiceDropdownComponent extends Component {
-  @tracked hiddenClass = 'lg:hidden'
+  @tracked buttonClass = 'bg-deep-blue'
+  @tracked hiddenClass = 'hidden'
+
+  @action hideDropdown() {
+    this.buttonClass = 'bg-deep-blue'
+    this.hiddenClass = 'hidden'
+  }
 
   @action showDropdown() {
+    this.buttonClass = 'bg-blue-500'
     this.hiddenClass = ''
   }
 
-  @action hideDropdown() {
-    this.hiddenClass = 'lg:hidden'
+  @action toggleDropdown() {
+    if (this.hiddenClass === '') {
+      this.hideDropdown()
+    } else {
+      this.showDropdown()
+    }
   }
 }
